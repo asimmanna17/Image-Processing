fd9843f921b7bf5b462362aaa4e54fcf3e26ff71
Asim Manna <67476183+asimmanna17@users.noreply.github.com>
2025-05-13 19:41:19 +0530

    Update dataset_patches.py

--- Codes/dataset_patches.py
+++ Codes/dataset_patches.py
@@ -3,9 +3,17 @@ from torch.utils.data import Dataset, DataLoader
 import os
 import random
 import torch
+import sys
 
 
 class DatasetNpy_Mix(Dataset):
+    # Each patch is saved in .npy file.
+    # .npy datatype: normalized, unified bayer pattern of BGGR
+    # ['sht']  [0:4] ldr [4:8] hdr
+    # ['mid']  [0:4] ldr [4:8] hdr
+    # ['lng']  [0:4] ldr [4:8] hdr
+    # ['hdr']  [0:4] hdr
+
     def __init__(self, data_folder, patch_size, training=True, num_patches_per_image=4):
         self.data_folder = data_folder
         self.img_list = os.listdir(data_folder)
@@ -19,40 +27,39 @@ class DatasetNpy_Mix(Dataset):
     def __getitem__(self, index):
         npypath = os.path.join(self.data_folder, self.img_list[index])
         imdata = np.load(npypath)
-        sht = imdata['sht']  # shape [8,H,W]
+
+        sht = imdata['sht']  # shape [8, H, W]
         mid = imdata['mid']
         lng = imdata['lng']
-        hdr = imdata['hdr']  # shape [4,H,W]
+        hdr = imdata['hdr']  # shape [4, H, W]
 
         if self.training:
             sht_patches, mid_patches, lng_patches, hdr_patches = [], [], [], []
 
             for _ in range(self.num_patches_per_image):
-                # Concatenate all channels for cropping
-                imstack = np.concatenate([sht, mid, lng, hdr], axis=0)
-                crop_stack = self.random_crop(imstack)
+                # Concatenate and crop
+                imstack = np.concatenate([sht, mid, lng, hdr], axis=0)  # [28, H, W]
+                crop = self.random_crop(imstack)  # [28, patch, patch]
 
-                sht_patches.append(self.to_tensor(crop_stack[0:8]))
-                mid_patches.append(self.to_tensor(crop_stack[8:16]))
-                lng_patches.append(self.to_tensor(crop_stack[16:24]))
-                hdr_patches.append(self.to_tensor(crop_stack[24:]))
+                sht_patches.append(self.to_tensor(crop[0:8]))
+                mid_patches.append(self.to_tensor(crop[8:16]))
+                lng_patches.append(self.to_tensor(crop[16:24]))
+                hdr_patches.append(self.to_tensor(crop[24:]))
 
-            # Stack patches into shape (num_patches, channels, H, W)
             return {
-                'sht': torch.stack(sht_patches),
+                'sht': torch.stack(sht_patches),  # [P, 8, H, W]
                 'mid': torch.stack(mid_patches),
                 'lng': torch.stack(lng_patches),
                 'hdr': torch.stack(hdr_patches)
             }
 
         else:
-            # Just convert all to tensor for eval
             return {
                 'sht': self.to_tensor(sht),
                 'mid': self.to_tensor(mid),
                 'lng': self.to_tensor(lng),
                 'hdr': self.to_tensor(hdr),
-                'save_name': os.path.splitext(self.img_list[index])[0]
+                'save_name': self.img_list[index].split('.')[0]
             }
 
     def to_tensor(self, np_array):
@@ -66,16 +73,23 @@ class DatasetNpy_Mix(Dataset):
         return np_array[:, h_start:h_start + self.patch_size, w_start:w_start + self.patch_size]
 
 
+def flatten_collate(batch):
+    sht = torch.cat([b['sht'] for b in batch], dim=0)  # [B*P, 8, H, W]
+    mid = torch.cat([b['mid'] for b in batch], dim=0)
+    lng = torch.cat([b['lng'] for b in batch], dim=0)
+    hdr = torch.cat([b['hdr'] for b in batch], dim=0)
+    return {'sht': sht, 'mid': mid, 'lng': lng, 'hdr': hdr}
+
+
 if __name__ == '__main__':
     folderpath = '/data/asim/ISP/HDR_transformer/data/RAW'
     dataset = DatasetNpy_Mix(data_folder=folderpath, patch_size=128, training=True, num_patches_per_image=4)
-    train_loader = DataLoader(dataset=dataset, batch_size=2, shuffle=True, num_workers=8)
+    train_loader = DataLoader(dataset=dataset, batch_size=2, shuffle=True, num_workers=8, collate_fn=flatten_collate)
 
     for i, cur_data in enumerate(train_loader):
-        sht = cur_data['sht']  # shape [1, num_patches, 8, 128, 128]
+        sht = cur_data['sht']  # [8, 8, 128, 128] if batch_size=2 and 4 patches/image
         mid = cur_data['mid']
         lng = cur_data['lng']
         hdr = cur_data['hdr']
 
         print(f"{i}: sht {sht.shape}, mid {mid.shape}, lng {lng.shape}, hdr {hdr.shape}")
-        
